be7929883b4cacc7105e900c3fc7e90e1fb87093
salaa <salaa@ANALYNSALA>
2025-03-17 11:14:34 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace LaundryManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.gridcus = new Bunifu.UI.WinForms.BunifuDataGridView();
+            this.addcus = new System.Windows.Forms.Button();
+            this.txtadd = new System.Windows.Forms.TextBox();
+            this.txtcon = new System.Windows.Forms.TextBox();
+            this.txtlname = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtfname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridcus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 98);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 501);
+            this.panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Franklin Gothic Demi", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(15, 277);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(171, 63);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Orders";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Franklin Gothic Demi", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(15, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 63);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Customer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Franklin Gothic Demi", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(15, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dashboard";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Aquamarine;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1105, 80);
+            this.panel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(343, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(484, 49);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Laundry Management";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.panel3.Controls.Add(this.gridcus);
+            this.panel3.Controls.Add(this.addcus);
+            this.panel3.Controls.Add(this.txtadd);
+            this.panel3.Controls.Add(this.txtcon);
+            this.panel3.Controls.Add(this.txtlname);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.txtfname);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(218, 98);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(899, 501);
+            this.panel3.TabIndex = 1;
+            this.panel3.Visible = false;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // gridcus
+            // 
+            this.gridcus.AllowCustomTheming = false;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(236)))), ((int)(((byte)(219)))));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
+            this.gridcus.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.gridcus.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridcus.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gridcus.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.gridcus.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.MediumSeaGreen;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI Semibold", 11.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(143)))), ((int)(((byte)(90)))));
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gridcus.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.gridcus.ColumnHeadersHeight = 40;
+            this.gridcus.CurrentTheme.AlternatingRowsStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(236)))), ((int)(((byte)(219)))));
+            this.gridcus.CurrentTheme.AlternatingRowsStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gridcus.CurrentTheme.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Black;
+            this.gridcus.CurrentTheme.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(209)))), ((int)(((byte)(169)))));
+            this.gridcus.CurrentTheme.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.gridcus.CurrentTheme.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.gridcus.CurrentTheme.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(232)))), ((int)(((byte)(212)))));
+            this.gridcus.CurrentTheme.HeaderStyle.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.gridcus.CurrentTheme.HeaderStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 11.75F, System.Drawing.FontStyle.Bold);
+            this.gridcus.CurrentTheme.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.gridcus.CurrentTheme.HeaderStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(143)))), ((int)(((byte)(90)))));
+            this.gridcus.CurrentTheme.HeaderStyle.SelectionForeColor = System.Drawing.Color.White;
+            this.gridcus.CurrentTheme.Name = null;
+            this.gridcus.CurrentTheme.RowsStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(239)))), ((int)(((byte)(226)))));
+            this.gridcus.CurrentTheme.RowsStyle.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            this.gridcus.CurrentTheme.RowsStyle.ForeColor = System.Drawing.Color.Black;
+            this.gridcus.CurrentTheme.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(209)))), ((int)(((byte)(169)))));
+            this.gridcus.CurrentTheme.RowsStyle.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(239)))), ((int)(((byte)(226)))));
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(209)))), ((int)(((byte)(169)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.gridcus.DefaultCellStyle = dataGridViewCellStyle6;
+            this.gridcus.EnableHeadersVisualStyles = false;
+            this.gridcus.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(232)))), ((int)(((byte)(212)))));
+            this.gridcus.HeaderBackColor = System.Drawing.Color.MediumSeaGreen;
+            this.gridcus.HeaderBgColor = System.Drawing.Color.Empty;
+            this.gridcus.HeaderForeColor = System.Drawing.Color.White;
+            this.gridcus.Location = new System.Drawing.Point(3, 194);
+            this.gridcus.Name = "gridcus";
+            this.gridcus.RowHeadersVisible = false;
+            this.gridcus.RowTemplate.Height = 40;
+            this.gridcus.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridcus.Size = new System.Drawing.Size(895, 304);
+            this.gridcus.TabIndex = 14;
+            this.gridcus.Theme = Bunifu.UI.WinForms.BunifuDataGridView.PresetThemes.MediumSeaGreen;
+            // 
+            // addcus
+            // 
+            this.addcus.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.addcus.Font = new System.Drawing.Font("Marlett", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addcus.Location = new System.Drawing.Point(744, 142);
+            this.addcus.Name = "addcus";
+            this.addcus.Size = new System.Drawing.Size(128, 40);
+            this.addcus.TabIndex = 13;
+            this.addcus.Text = "Insert";
+            this.addcus.UseVisualStyleBackColor = false;
+            this.addcus.Click += new System.EventHandler(this.addcus_Click);
+            // 
+            // txtadd
+            // 
+            this.txtadd.Location = new System.Drawing.Point(382, 159);
+            this.txtadd.Multiline = true;
+            this.txtadd.Name = "txtadd";
+            this.txtadd.Size = new System.Drawing.Size(301, 23);
+            this.txtadd.TabIndex = 12;
+            // 
+            // txtcon
+            // 
+            this.txtcon.Location = new System.Drawing.Point(12, 159);
+            this.txtcon.Multiline = true;
+            this.txtcon.Name = "txtcon";
+            this.txtcon.Size = new System.Drawing.Size(275, 23);
+            this.txtcon.TabIndex = 11;
+            // 
+            // txtlname
+            // 
+            this.txtlname.Location = new System.Drawing.Point(382, 91);
+            this.txtlname.Multiline = true;
+            this.txtlname.Name = "txtlname";
+            this.txtlname.Size = new System.Drawing.Size(301, 23);
+            this.txtlname.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(378, 133);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 23);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(8, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 23);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Contact Info";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(378, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Lastname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Firstname";
+            // 
+            // txtfname
+            // 
+            this.txtfname.Location = new System.Drawing.Point(12, 89);
+            this.txtfname.Multiline = true;
+            this.txtfname.Name = "txtfname";
+            this.txtfname.Size = new System.Drawing.Size(301, 25);
+            this.txtfname.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Demi", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(244, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(343, 39);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Customer Information";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1129, 611);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridcus)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtfname;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtlname;
+        private System.Windows.Forms.Button addcus;
+        private System.Windows.Forms.TextBox txtadd;
+        private System.Windows.Forms.TextBox txtcon;
+        private Bunifu.UI.WinForms.BunifuDataGridView gridcus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+
+using System.Windows.Forms;
+
+namespace LaundryManagement
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conn;
+        OleDbCommand cmd;
+        
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            
+            dbcon();
+            GetCustomer();
+        }
+         
+        public void dbcon()
+        {
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\salaa\\OneDrive\\Documents\\Advance Database\\Laundry Management.accdb;";
+            conn = new OleDbConnection(connectionString);
+        }
+        public void GetCustomer()
+        {
+
+            try
+            {
+                string query = "SELECT Customer_fname as Firstname, Customer_lname as Lastname, Contact_Info as Information, Address  from Customer order by Customer_Id asc";
+                using (OleDbDataAdapter adapt = new OleDbDataAdapter(query, conn))
+                {
+                    dt.Clear();
+                    adapt.Fill(dt);
+                    gridcus.DataSource = dt;
+                }
+            }
+            catch (OleDbException ex)
+            {
+                MessageBox.Show($"Database error: {ex.Message}");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading Author: {ex.Message}");
+            }
+        }
+
+
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel3.Visible = true;
+            panel3.BringToFront();
+        }
+
+        private void addcus_Click(object sender, EventArgs e)
+        {
+             if (string.IsNullOrEmpty(txtfname.Text) || string.IsNullOrEmpty(txtlname.Text) ||
+                string.IsNullOrEmpty(txtcon.Text) || string.IsNullOrEmpty(txtadd.Text))
+            {
+                MessageBox.Show("Please fill in all fields."); // Show error if any field is empty
+                return; // Exit the method
+            }
+             //Validate that contact info is numeric
+             if (!long.TryParse(txtcon.Text, out _))
+            {
+                MessageBox.Show("Contact info must be numeric.");
+                return;
+            }
+
+            string query = "INSERT INTO Customer (Customer_fname,Customer_lname, Contact_Info, Address) VALUES (@firstname, @lastname, @contactinfo, @address)";
+            cmd = new OleDbCommand(query, conn);
+            cmd.Parameters.AddWithValue("@Customer_fname", txtfname.Text);
+            cmd.Parameters.AddWithValue("@Customer_lname", txtlname.Text);
+            cmd.Parameters.AddWithValue("@Contact_Info", txtcon.Text);
+            cmd.Parameters.AddWithValue("Address", txtadd.Text);
+
+            try
+            {
+                conn.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                MessageBox.Show(rowsAffected > 0 ? "Customer inserted successfully!" : "Failed to add record.");
+                if (rowsAffected > 0)
+                {
+                    txtfname.Text = txtlname.Text = txtcon.Text = txtadd.Text = "";
+                    dt.Clear();
+                    GetCustomer();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+       
+    }
+    }
+
